826b547c5aa3649110e84a109775e4f6bfc79fd9
Spencer Finnell <spencerfinnell@gmail.com>
2014-09-16 14:28:40 -0400

    Dont use @import.

--- functions.php
+++ functions.php
@@ -6,3 +6,8 @@
  *
  * @since Marketify Child 1.0
  */
+
+function marketify_child_styles() {
+    wp_enqueue_style( 'marketify-child', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'marketify_child_styles', 20 );
